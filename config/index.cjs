96d7abee63c8e4d4e33b91bc09795a9c4b448a72
YMM520520 <118239090+YMM520520@users.noreply.github.com>
2022-11-15 12:59:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -50,7 +50,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'tIwtkV2WG9QuUPc8IFDG0wmYOTc-JjaEBlT5Nk1RnwI',
+  CALLBACK_TEMPLATE_ID: '	KHBzsXfMHrD5n-wSni20P2Eb7E1j7gU_75CaYMoWcH0',
 
   CALLBACK_USERS: [
     {
